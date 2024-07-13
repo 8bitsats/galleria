382676e82ee9a49b5ae0a137d8827a30bc00b779
8 bit <8bit@ordlibrary.com>
2024-07-13 18:58:04 -0400

    ffff

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -36,7 +36,7 @@ const Hero = () => {
 
       <div className="text-center">
         <h1 className="bg-gradient-to-r from-white via-accent to-primary bg-clip-text text-5xl font-bold tracking-tighter text-transparent sm:text-7xl/none">
-         The Old Library Solana Portfolio Platform
+     OrdLibrary Portfolio Platform
         </h1>
         <p className="mt-6 text-base leading-8 text-gray-300 sm:text-lg">
 Welcome to the Ord Library Solana NFT Portfolio Platform
